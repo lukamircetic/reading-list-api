262013df31eac00e9f2a29ee58523efe3a5707b3
lukamircetic <luka.mircetic@gmail.com>
2025-03-20 13:56:38 -0400

    added start of post request

--- internal/server/articles.go
+++ internal/server/articles.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"errors"
 	"net/http"
 	"reading-list-api/internal/types"
 
@@ -41,5 +42,39 @@ func NewArticleResponse(article *types.Article) *ArticleResponse {
 }
 
 func (rd *ArticleResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *Server) CreateArticle(w http.ResponseWriter, r *http.Request) {
+	// 1 - extract the article link from the request body
+	data := &ArticleRequest{}
+	err := render.Bind(r, data)
+	if err != nil {
+		render.Render(w, r, ErrInvalidRequest((err)))
+		return
+	}
+	// 1.5 - check if the link already exists in the db
+
+	// articleLink := data.ArticleLink
+	// err := s.db.articleExists(articleLink)
+
+	// 2 - send the article for extraction to gemini
+	// 3 - parse the gemini response (some form of json)
+	// 4 - return if the link isn't a article
+	// 5 - create a db record for this article and populate all the fields
+	// 6 - return 200
+}
+
+type ArticleRequest struct {
+	ArticleLink string `json:"articleLink"`
+}
+
+func (a *ArticleRequest) Bind(r *http.Request) error {
+	// a.ArticleLink is nil if no Article fields are sent in the request. Return an
+	// error to avoid a nil pointer dereference.
+	if a.ArticleLink == "" {
+		return errors.New("missing required Article fields")
+	}
+
 	return nil
 }
